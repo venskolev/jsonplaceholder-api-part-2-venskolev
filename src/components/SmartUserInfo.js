02fc36b17337e5ad53b043b0f5e5150ba12e2280
Vens <v.kolev@alfatrex.com>
2022-02-24 14:59:13 +0100

    Fix and add photo

--- src/components/SmartUserInfo.js
+++ src/components/SmartUserInfo.js
@@ -9,13 +9,16 @@ export const SmartUserInfo = () => {
   const [user, setUser] = useState(null);
   const [todos, setTodos] = useState([]);
   const params = useParams();
+  const [userAlbum, setUserAlbum] = useState(null);
 
   useEffect(() => {
     // Variante 1: Funktion definieren und später mit Namen aufrufen
     const loadTodos = async () => {
       try {
         const response = await axios.get("/users/" + params.userId + "/todos");
+        const album = await axios.get("/photos/");
         setTodos(response.data);
+        setUserAlbum(album.data);
       } catch (error) {
         console.log(error);
       }
@@ -25,6 +28,8 @@ export const SmartUserInfo = () => {
       (async () => {
         try {
           const response = await axios.get("/users/" + params.userId);
+          const album = await axios.get("/photos/" + params.userId);
+          setUserAlbum(album.data);
           setUser(response.data);
         } catch (error) {
           console.log(error);
@@ -33,16 +38,26 @@ export const SmartUserInfo = () => {
       loadTodos();
     }
   }, [params.userId]);
+  // function userBild(e) {
+  //   e.target.setAttribute( 'src', `${userAlbum.url}`);
+  //   e.target.setAttribute('alt', 'UserBild');
+  // };
   return (
     <div className="UserInfo">
       <header className="App-header">
           <>
-            <img src={logo} className="App-logo" alt="logo" />
-            <span>Lade Daten</span>
+           <a href="/"><img src={logo} className="App-logo" alt="logo" />
+            <span>Lade Daten</span></a>
             <button className="ButtonHome"><a href="/">Home</a></button>
           </>
       </header>
       <h1>SmartUserInfo</h1>
+      {userAlbum ? (
+        <div>
+            <img src={userAlbum.thumbnailUrl} alt="Bild"/>
+          {/* <span>{userAlbum.title}</span> */}
+        </div>
+      ) : undefined}
       <p>{params.userId}</p>
       {user ? (
         <>
